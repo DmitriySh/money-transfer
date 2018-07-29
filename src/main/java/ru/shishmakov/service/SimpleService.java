c0fb78441a78b774bb685c550c2ce835bcf4dac0
DmitriySh <dashishmakov@gmail.com>
2018-07-29 23:47:10 +0300

    Add Java 9 support and Lombok

--- src/main/java/ru/shishmakov/service/SimpleService.java
+++ src/main/java/ru/shishmakov/service/SimpleService.java
@@ -1,14 +1,16 @@
 package ru.shishmakov.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
+@Slf4j
 @Service
 public class SimpleService {
 
     @PostConstruct
     public void init() {
-        System.out.println("Hello world Spring Boot!");
+        log.info("Hello world Spring Boot!");
     }
 }

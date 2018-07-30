d62757fdd5e920a2cc136e387c41b3c0b5937e7f
DmitriySh <dashishmakov@gmail.com>
2018-07-30 21:14:44 +0300

    Handle all exceptions into AccountService

--- src/main/java/ru/shishmakov/controller/AccountService.java
+++ src/main/java/ru/shishmakov/controller/AccountService.java
@@ -24,45 +24,62 @@ public class AccountService {
     }
 
     public Optional<Account> getAccount(long accNumber) {
-        log.debug("get: {}", accNumber);
+        log.debug("get account: {}", accNumber);
         return repository.findByAccNumber(accNumber);
     }
 
     @Transactional(rollbackFor = Exception.class)
-    public Account withdraw(long number, BigDecimal amount) {
-        checkAmount(amount);
-        Account account = repository.findByAccNumberAndLock(number).orElseThrow(() -> new IllegalArgumentException("not found id: " + number));
-        decreaseAmount(account, amount);
+    public Optional<Account> withdraw(long number, BigDecimal amount) {
+        try {
+            checkAmount(amount);
+            Account account = repository.findByAccNumberAndLock(number)
+                    .orElseThrow(() -> new IllegalArgumentException("not found id: " + number));
+            decreaseAmount(account, amount);
 
-        log.debug("withdraw account: {}, amount: {}", number, amount);
-        return repository.save(account);
+            log.debug("withdraw account: {}, amount: {}", number, amount);
+            return Optional.of(repository.save(account));
+        } catch (Exception e) {
+            log.error("withdraw error", e);
+            return Optional.empty();
+        }
     }
 
     @Transactional(rollbackFor = Exception.class)
-    public Account deposit(long number, BigDecimal amount) {
-        checkAmount(amount);
-        Account account = repository.findByAccNumberAndLock(number).orElseThrow(() -> new IllegalArgumentException("not found id: " + number));
-        increaseAmount(account, amount);
+    public Optional<Account> deposit(long number, BigDecimal amount) {
+        try {
+            checkAmount(amount);
+            Account account = repository.findByAccNumberAndLock(number)
+                    .orElseThrow(() -> new IllegalArgumentException("not found id: " + number));
+            increaseAmount(account, amount);
 
-        log.debug("deposit account: {}, amount: {}", number, amount);
-        return repository.save(account);
+            log.debug("deposit account: {}, amount: {}", number, amount);
+            return Optional.of(repository.save(account));
+        } catch (Exception e) {
+            log.error("deposit error", e);
+            return Optional.empty();
+        }
     }
 
     @Transactional(rollbackFor = Exception.class)
-    public List<Account> transfer(long from, long to, BigDecimal amount) {
-        checkAmount(amount);
-        long first = from < to ? from : to;
-        long second = from < to ? to : from;
+    public Optional<List<Account>> transfer(long from, long to, BigDecimal amount) {
+        try {
+            checkAmount(amount);
+            long first = from < to ? from : to;
+            long second = from < to ? to : from;
 
-        Account firstAccount = repository.findByAccNumberAndLock(first)
-                .orElseThrow(() -> new IllegalArgumentException("not found id: " + first));
-        Account secondAccount = repository.findByAccNumberAndLock(second)
-                .orElseThrow(() -> new IllegalArgumentException("not found id: " + second));
-        decreaseAmount(firstAccount.getAccNumber().equals(from) ? firstAccount : secondAccount, amount);
-        increaseAmount(firstAccount.getAccNumber().equals(to) ? firstAccount : secondAccount, amount);
+            Account firstAccount = repository.findByAccNumberAndLock(first)
+                    .orElseThrow(() -> new IllegalArgumentException("not found id: " + first));
+            Account secondAccount = repository.findByAccNumberAndLock(second)
+                    .orElseThrow(() -> new IllegalArgumentException("not found id: " + second));
+            decreaseAmount(firstAccount.getAccNumber().equals(from) ? firstAccount : secondAccount, amount);
+            increaseAmount(firstAccount.getAccNumber().equals(to) ? firstAccount : secondAccount, amount);
 
-        log.debug("transfer amount: {}, accounts: {} -> {} ", amount, from, to);
-        return repository.saveAll(List.of(firstAccount, secondAccount));
+            log.debug("transfer amount: {}, accounts: {} -> {} ", amount, from, to);
+            return Optional.of(repository.saveAll(List.of(firstAccount, secondAccount)));
+        } catch (Exception e) {
+            log.error("transfer error", e);
+            return Optional.empty();
+        }
     }
 
     private static void increaseAmount(Account account, BigDecimal amount) {
--- src/main/java/ru/shishmakov/controller/ApiController.java
+++ src/main/java/ru/shishmakov/controller/ApiController.java
@@ -31,13 +31,9 @@ public class ApiController {
 
     @PutMapping("/accounts/transfer")
     public ResponseEntity<List<Account>> transfer(@RequestBody Transfer transfer) {
-        try {
-            List<Account> accounts = service.transfer(requireNonNull(transfer.getFrom()), requireNonNull(transfer.getTo()), transfer.getAmount());
-            return new ResponseEntity<>(accounts, OK);
-        } catch (Exception e) {
-            log.error("transfer error", e);
-            return new ResponseEntity<>(BAD_REQUEST);
-        }
+        return service.transfer(requireNonNull(transfer.getFrom()), requireNonNull(transfer.getTo()), transfer.getAmount())
+                .map(a -> new ResponseEntity<>(a, OK))
+                .orElseGet(() -> new ResponseEntity<>(BAD_REQUEST));
     }
 
     @GetMapping("/account/{accNumber}")
@@ -49,23 +45,15 @@ public class ApiController {
 
     @PutMapping("/account/deposit")
     public ResponseEntity<Account> deposit(@RequestBody Transfer transfer) {
-        try {
-            Account account = service.deposit(requireNonNull(transfer.getTo()), transfer.getAmount());
-            return new ResponseEntity<>(account, OK);
-        } catch (Exception e) {
-            log.error("transfer error", e);
-            return new ResponseEntity<>(BAD_REQUEST);
-        }
+        return service.deposit(requireNonNull(transfer.getTo()), transfer.getAmount())
+                .map(a -> new ResponseEntity<>(a, OK))
+                .orElseGet(() -> new ResponseEntity<>(BAD_REQUEST));
     }
 
     @PutMapping("/account/withdraw")
     public ResponseEntity<Account> withdraw(@RequestBody Transfer transfer) {
-        try {
-            Account account = service.withdraw(requireNonNull(transfer.getFrom()), transfer.getAmount());
-            return new ResponseEntity<>(account, OK);
-        } catch (Exception e) {
-            log.error("transfer error", e);
-            return new ResponseEntity<>(BAD_REQUEST);
-        }
+        return service.withdraw(requireNonNull(transfer.getFrom()), transfer.getAmount())
+                .map(a -> new ResponseEntity<>(a, OK))
+                .orElseGet(() -> new ResponseEntity<>(BAD_REQUEST));
     }
 }

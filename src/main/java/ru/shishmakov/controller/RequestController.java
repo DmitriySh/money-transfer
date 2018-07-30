011b163cce87a4282f29a4abbc46a3de9a4f9aff
DmitriySh <dashishmakov@gmail.com>
2018-07-30 16:57:57 +0300

    Make AccountRepository like an interface

--- src/main/java/ru/shishmakov/controller/RequestController.java
+++ src/main/java/ru/shishmakov/controller/RequestController.java
@@ -2,6 +2,7 @@ package ru.shishmakov.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.shishmakov.dao.AccountRepository;
 import ru.shishmakov.model.Account;
@@ -9,6 +10,9 @@ import ru.shishmakov.model.Transfer;
 
 import java.math.BigDecimal;
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -27,25 +31,33 @@ public class RequestController {
         long from = transfer.getFrom();
         long to = transfer.getTo();
         BigDecimal amount = transfer.getAmount();
+        log.debug("transfer:{}, {} -> {} ", amount, from, to);
+        // TODO: 30.07.2018 impl
     }
 
-    @GetMapping("/account")
-    public List<Account> getAccount() {
-        return repository.findAll();
+    @GetMapping("/accounts")
+    public ResponseEntity<List<Account>> getAccounts() {
+        log.debug("get all accounts");
+        return new ResponseEntity<>(repository.findAll(), OK);
     }
 
     @GetMapping("/account/{id}")
-    public Account getAccount(@PathVariable("id") long id) {
-        return repository.findById(id);
+    public ResponseEntity<Account> getAccount(@PathVariable("id") long id) {
+        log.debug("get account: {}", id);
+        return repository.findById(id)
+                .map(a -> new ResponseEntity<>(a, OK))
+                .orElseGet(() -> new ResponseEntity<>(NOT_FOUND));
     }
 
-    @PutMapping("/account/{id}/put")
-    public void putUp(@PathVariable("id") long id) {
-
+    @PutMapping("/account/{id}/put/{amount}")
+    public void putUp(@PathVariable("id") long id, @PathVariable BigDecimal amount) {
+        log.debug("put account: {}, amount: {}", id, amount);
+        // TODO: 30.07.2018 impl
     }
 
-    @PutMapping("/account/{id}/withdraw")
-    public void withdraw(@RequestBody Transfer transfer) {
-
+    @PutMapping("/account/{id}/withdraw/{amount}")
+    public void withdraw(@PathVariable("id") long id, @PathVariable BigDecimal amount) {
+        log.debug("withdraw account: {}, amount: {}", id, amount);
+        // TODO: 30.07.2018 impl
     }
 }
--- src/main/java/ru/shishmakov/dao/AccountRepository.java
+++ src/main/java/ru/shishmakov/dao/AccountRepository.java
@@ -1,25 +1,12 @@
 package ru.shishmakov.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.shishmakov.model.Account;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
-
 @Repository
 @Transactional(readOnly = true)
-public class AccountRepository {
-    @PersistenceContext
-    private EntityManager em;
+public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    public Account findById(long id) {
-        return em.find(Account.class, id);
-    }
-
-    public List<Account> findAll() {
-        return em.createQuery("SELECT a FROM Account a", Account.class)
-                .getResultList();
-    }
 }

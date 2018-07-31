5636bada98e63443cd36b034572f467d9e5cf9fd
DmitriySh <dashishmakov@gmail.com>
2018-07-31 11:34:25 +0300

    Add FK for log table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,5 +13,7 @@ CREATE TABLE IF NOT EXISTS log
   to_number BIGINT,
   amount DECIMAL(20, 2) NOT NULL,
   description VARCHAR(100) NOT NULL,
-  date TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp
+  date TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+  FOREIGN KEY (from_number) REFERENCES account(acc_number),
+  FOREIGN KEY (to_number) REFERENCES account(acc_number),
 );
